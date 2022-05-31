b944fdff21fc6c5af809473f2fe5c185a8d52990
Ahmed Gadein Obied <ahmed.g.obied@gmail.com>
2022-05-31 12:13:26 +0200

    Settings Screen (#20)

--- app/src/main/java/com/dinder/rihla/rider/ui/home/trips/TripsFragment.kt
+++ app/src/main/java/com/dinder/rihla/rider/ui/home/trips/TripsFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dinder.rihla.rider.R
 import com.dinder.rihla.rider.adapter.DestinationAdapter
@@ -16,6 +17,7 @@ import com.dinder.rihla.rider.adapter.TripAdapter
 import com.dinder.rihla.rider.common.RihlaFragment
 import com.dinder.rihla.rider.data.model.Destination
 import com.dinder.rihla.rider.databinding.TripsFragmentBinding
+import com.dinder.rihla.rider.ui.home.HomeFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -38,6 +40,12 @@ class TripsFragment : RihlaFragment() {
     }
 
     private fun setUI() {
+        binding.settingsButton.setOnClickListener {
+            findNavController().navigate(
+                HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
+            )
+        }
+
         val tripsAdapter = TripAdapter()
         binding.tripsRecyclerView.apply {
             adapter = tripsAdapter
