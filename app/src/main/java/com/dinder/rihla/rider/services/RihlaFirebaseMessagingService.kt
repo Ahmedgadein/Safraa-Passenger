e05ac52a4339d133894988bcf9703e1952af281f
Ahmed Gadein Obied <ahmed.g.obied@gmail.com>
2022-08-29 21:14:42 +0200

    handle foreground notifications (#47)

--- app/src/main/java/com/dinder/rihla/rider/services/RihlaFirebaseMessagingService.kt
+++ app/src/main/java/com/dinder/rihla/rider/services/RihlaFirebaseMessagingService.kt
@@ -1,9 +1,45 @@
 package com.dinder.rihla.rider.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.dinder.rihla.rider.MainActivity
+import com.dinder.rihla.rider.R
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class RihlaFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d("notification", "onMessageReceived: " + message.data["message"])
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
+        val channelId = "Default"
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_rihla_new)
+            .setContentTitle(message.notification?.title)
+            .setContentText(message.notification?.body)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Default channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(channel)
+        }
+        manager.notify(0, builder.build())
     }
 }
