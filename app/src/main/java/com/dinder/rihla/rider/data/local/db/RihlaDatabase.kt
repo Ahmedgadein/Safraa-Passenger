7881894b88d90fcec84c2f3ed223eacced00614b
Ahmedgadein <ahmed.g.obied@gmail.com>
2022-09-14 18:24:01 +0200

    profile screen update

--- app/src/main/java/com/dinder/rihla/rider/data/local/db/RihlaDatabase.kt
+++ app/src/main/java/com/dinder/rihla/rider/data/local/db/RihlaDatabase.kt
@@ -2,13 +2,15 @@ package com.dinder.rihla.rider.data.local.db
 
 import android.content.Context
 import androidx.room.* // ktlint-disable no-wildcard-imports
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.dinder.rihla.rider.common.Constants.DATABASE_NAME
 import com.dinder.rihla.rider.data.local.UserDao
 import com.dinder.rihla.rider.data.model.User
 
 @Database(
     entities = [User::class],
-    version = 2,
+    version = 3,
     autoMigrations = [AutoMigration(from = 1, to = 2)]
 )
 @TypeConverters(Converters::class)
@@ -27,7 +29,14 @@ abstract class RihlaDatabase : RoomDatabase() {
 
         private fun buildDatabase(context: Context): RihlaDatabase {
             return Room.databaseBuilder(context, RihlaDatabase::class.java, DATABASE_NAME)
+                .addMigrations(MIGRATION_2_3)
                 .build()
         }
+    }
+}
+
+val MIGRATION_2_3 = object : Migration(2, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE user ADD COLUMN points INTEGER NOT NULL DEFAULT 0")
     }
 }
