1273c757dac55e4ca58de01f7a9a8a0cc1a7594d
Ahmedgadein <ahmed.g.obied@gmail.com>
2022-11-22 08:08:38 +0200

    update twitter link

--- app/src/main/java/com/dinder/rihla/rider/ui/update/UpdateAppFragment.kt
+++ app/src/main/java/com/dinder/rihla/rider/ui/update/UpdateAppFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.dinder.rihla.rider.R
 import com.dinder.rihla.rider.databinding.FragmentUpdateAppBinding
 
 class UpdateAppFragment : Fragment() {
